a4fb3b791bbd05e0b79bf77310c673e8480f8b35
Jorge Pessoa <jpessoa.on@gmail.com>
2016-03-11 22:47:09 +0000

    Added Generic Visitor interface
    Allows for implementation of Visitors for File objects (subclasses) with
    generic return types.
    
    Uses parameterized types to allow different return types. When
    implementing the interface the return type must be specified as the
    parameter of the interface

---
+++ src/main/java/pt/tecnico/myDrive/visitors/GenericVisitor.java
@@ -0,0 +1,13 @@
+package pt.tecnico.myDrive.visitors;
+
+import pt.tecnico.myDrive.domain.App;
+import pt.tecnico.myDrive.domain.Directory;
+import pt.tecnico.myDrive.domain.Link;
+import pt.tecnico.myDrive.domain.PlainFile;
+
+public interface GenericVisitor<T> {
+	public T visit(Directory dir);
+	public T visit(PlainFile pf);
+	public T visit(Link l);
+	public T visit(App a);	
+}
