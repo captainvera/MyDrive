5520f89577a6058b0e843f79969bac7e8101238f
André Santos <ags.rndd@gmail.com>
2016-03-12 19:02:18 +0000

    Added functionality to Directory
    - Added path support. It's now possible to get the path of a directory.
    - Added file getting within the directory. It's now possible to return a file
      inside a directory by its name.
    - Added a checker to determine whether of not a directory is in the top-level.
      It can be used to check whether or not a directory is root.

--- src/main/java/pt/tecnico/myDrive/domain/Directory.java
+++ src/main/java/pt/tecnico/myDrive/domain/Directory.java
@@ -1,6 +1,7 @@
 package pt.tecnico.myDrive.domain;
 
 import pt.tecnico.myDrive.visitors.GenericVisitor;
+import pt.tecnico.myDrive.exceptions.FileUnknownException;
 
 public class Directory extends Directory_Base {
 
@@ -8,9 +9,46 @@ public class Directory extends Directory_Base {
     super();
   }
 
+  /**
+   * A top-level directory is a directory which its parent is itself.
+   * An example of a top-level directory would be the root directory.
+   *
+   * @return true if the directory is a top-level directory.
+   */
+  public boolean isTopLevelDirectory() { return getParent() == this; }
+
+  /**
+   * The path of a directory is a string that specifies how to reach itself by
+   * going through other directories in a filesystem.
+   *
+   * @return The string corresponding to the path the directory.
+   */
+  public String getPath() { return isTopLevelDirectory() ? getName() : getPathHelper(); }
+
+  /**
+   * Simple helper function to call when the path needs to be processed
+   */
+  private String getPathHelper() {
+    return (isTopLevelDirectory()) ? "" : getParent().getPath();
+  }
+
+  /**
+   * Get a file by its name.
+   *
+   * @param filename
+   * @return The file which name is filename.
+   * @throws FileUnknownException
+   */
+  public File getFileByName(String filename) throws FileUnknownException {
+    for (File file : getFileSet())
+      if (filename.equals(file.getName()))
+        return file;
+    throw new FileUnknownException(filename);
+  }
+
   @Override
   public <T> T accept(GenericVisitor<T> v){
-	  return v.visit(this);
+    return v.visit(this);
   }
 
 }
