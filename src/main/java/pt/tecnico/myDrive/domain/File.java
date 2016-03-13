b6d9d880d635e30bf847932e5aafabf5271bd46c
Jorge Pessoa <jpessoa.on@gmail.com>
2016-03-13 02:26:30 +0000

    Added open() and remove() methods
    open() method should be implemented in subclasses with proper behaviour
    for opening that file. Provides a signle method to open any kind of file
    
    remove() should be used for removal of file. Subclasses may Override
    method for proper behaviour

--- src/main/java/pt/tecnico/myDrive/domain/File.java
+++ src/main/java/pt/tecnico/myDrive/domain/File.java
@@ -8,10 +8,31 @@ public abstract class File extends File_Base {
     super();
 	}
 
+	protected void init(String name, Directory parent, Integer id){
+		setName(name);
+		setParent(parent);
+		setId(id);
+	}
+
 	public String toString(){
 		return this.getName();
 	}
 
+	/**
+	 * Basic remove implementation for FIle objects
+	 * Specific sublcass behaviour might be implemented 
+	 */
+	public void remove(){
+		setFileSystem(null);
+		setParent(null);	
+		
+	}
+	
+	/**
+	 * Opens the file. Should be implemented with proper behaviour in subclasses
+	 */
+	public abstract void open();
+	
 	public abstract int getSize();
 
   public abstract <T> T accept(GenericVisitor<T> v);
