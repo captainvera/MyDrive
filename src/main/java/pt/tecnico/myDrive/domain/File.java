b22237e018ea279fc608233b11cc5a85ad9d24f0
André Santos <ags.rndd@gmail.com>
2016-03-13 02:03:53 +0000

    Added path support to files.
    Previously there was no support to get the path of a file, hence, exporting to
    XML could be cumbersome.

--- src/main/java/pt/tecnico/myDrive/domain/File.java
+++ src/main/java/pt/tecnico/myDrive/domain/File.java
@@ -6,15 +6,25 @@ public abstract class File extends File_Base {
 
   public File() {
     super();
-	}
+  }
 
-	public String toString(){
-		return this.getName();
-	}
+  public String toString(){
+    return this.getName();
+  }
 
-	public abstract int getSize();
+  public abstract int getSize();
 
   public abstract <T> T accept(GenericVisitor<T> v);
 
   public abstract void remove();
+
+  /**
+   * The path of a file is the concatenation of the path of the enclosing
+   * directory and its name.
+   *
+   * @return The path corresponding to its location in the filesystem.
+   */
+  public String getPath() { return getParent().getPath() + getName(); }
+
 }
+
