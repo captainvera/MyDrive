eef2a33350a796048bd784ffaadf1dd74c2e2bb5
Jorge Pessoa <jpessoa.on@gmail.com>
2016-03-14 15:41:38 +0000

    Reworked Main to match necessary functionality
    Still split in two functions to test persistency. Should be stored into
    one

--- src/main/java/pt/tecnico/myDrive/myDriveApplication.java
+++ src/main/java/pt/tecnico/myDrive/myDriveApplication.java
@@ -30,9 +30,9 @@ public class myDriveApplication {
 		System.out.println("-- Welcome to myDrive");
 		try {
 			// if(args.length == 0) setupDrive();
-			System.out.println("-- Setup returning to main");
-			// UNCOMMENT TO TEST OBJECT PERSISTENCE!
-			testRun();
+			System.out.println("-- setupDrive() return to main");
+		  System.out.println("-- executing testRun()");  
+      testRun();
 		} finally {
 			// ensure an orderly shutdown
 			FenixFramework.shutdown();
@@ -42,53 +42,50 @@ public class myDriveApplication {
 	@Atomic
 	public static void setupDrive(){
 		/**
-		 * Temporary main with basic tests
-		 * TODO: Change to match necessary description
+		 * Basic setup to test desired functionality 
 		 */
 
-		System.out.println("Setup: " + FenixFramework.getDomainRoot());
+		System.out.println("-- Setting root: " + FenixFramework.getDomainRoot());
 
 		FileSystem fs = FileSystem.getInstance();
 
 		try{
 			fs.login("root","***");
 		}catch(Exception e){
-			System.out.println("PROBLEMO LOGIN");
+			System.out.println("-- Couldn't login with root!");
 		}
 
-		try{
+		/*
+     * Criação do ficheiro README mudando o working directory 
+     * */
+    try{
+      System.out.println("-- Showing Path");
 			System.out.println(fs.listPath());
-			fs.changeDirectory("home");
-			System.out.println(fs.listPath());
-			fs.changeDirectory("root");
-		}catch(Exception e){
-			System.out.println("CD ERROR");
-		}
-		PlainFile readme = fs.createPlainFile("README");
-		try{
+
+      System.out.println("-- Creating README");
+		  PlainFile readme = fs.createPlainFile("README");
+      readme.setData("Lista de utilizadores");
+      System.out.println("-- Listing current Directory");
 			System.out.println(fs.listDirectory());
-		} catch(Exception e){
+      System.out.println("-- Showing result of opening README");
+      System.out.println(fs.executeFile("README"));
+	  }catch(Exception e){
+			System.out.println("-- COuldn't create README!");
+		}
+		
+    /*
+     * Criação do caminho /usr/local/bin absolutamente
+     */
+    try{
+      System.out.println("-- Creating /usr/local/bin by path");
+      fs.createDirectoryByPath("/usr/local/bin");
+      System.out.println("-- Listing /usr/local");
+      System.out.println(fs.listFileByPathSimple("/usr/local"));
+    } catch(Exception e){
 			System.out.println(e.getMessage());
 		}
-		System.out.println(fs.listPath());
-		System.out.println(fs.currentDirectory());
-		try{
-			fs.createDirectory("test");
-			fs.changeDirectory("test");
-			System.out.println(fs.listPath());
-			fs.changeDirectory("..");
-			System.out.println(fs.listPath());
-			fs.changeDirectory(".");
-			System.out.println(fs.listPath());
-			fs.changeDirectory("..");
-			System.out.println(fs.listPath());
-			fs.changeDirectory("..");
-			System.out.println(fs.listPath());
-			fs.changeDirectory("..");
-		}catch(Exception e){
-		}	
 
-		System.out.println("Successful setup!");
+		System.out.println("-- Successful default setup!");
 	}
 
 	@Atomic
@@ -97,10 +94,24 @@ public class myDriveApplication {
 
 		FileSystem fs = FileSystem.getInstance();
 		try{
-			fs.login("root","***");
+      fs.login("root","***");
+      System.out.println("-- Showing path");
+			System.out.println(fs.listPath());
+      System.out.println("-- Listing current Directory");
 			System.out.println(fs.listDirectory());
-		}catch(Exception e){
-			System.out.println("ERROR!");
+
+
+      System.out.println("-- Removing README");
+      fs.removeFileByPath("/home/root/README");
+      System.out.println("-- Listing current Directory");
+			System.out.println(fs.listDirectory());
+
+      System.out.println("-- Listing /user/local");
+      System.out.println(fs.listFileByPathSimple("/usr/local"));
+
+    }catch(Exception e){
+      System.out.println("-- Exception on testRun!");
+			System.out.println(e.getMessage());
 		}
 	}
 
