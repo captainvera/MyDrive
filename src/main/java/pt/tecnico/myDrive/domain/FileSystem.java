0780d9f5a8c533adc5abe5e19b05c79daed3bd8e
Pedro Ascensao <rafascen@gmail.com>
2016-03-14 07:09:01 +0000

    FileSystem added import and export

--- src/main/java/pt/tecnico/myDrive/domain/FileSystem.java
+++ src/main/java/pt/tecnico/myDrive/domain/FileSystem.java
@@ -27,6 +27,8 @@ import org.apache.logging.log4j.Logger;
 
 import pt.tecnico.myDrive.visitors.DirectoryVisitor;
 
+import java.util.*;
+
 public class FileSystem extends FileSystem_Base {
 
 
@@ -367,81 +369,55 @@ public class FileSystem extends FileSystem_Base {
 		}
 
 	public Document xmlExport(){
-		DirectoryVisitor visitor = new DirectoryVisitor();
 		XMLExporterVisitor xml = new XMLExporterVisitor();
 		Element mydrive = new Element("myDrive");
 		Document doc = new Document(mydrive);
 
 		for(User u: getUsersSet())
 			mydrive.addContent(u.xmlExport());
-
 		for (File f: getFilesSet())
 			mydrive.addContent(f.accept(xml));
 
 		return doc;
 	}
 
-	/*public void xmlImport(Element firstElement){
+	public void xmlImport(Element firstElement){
 		try{
-		for(Element userElement: firstElement.getChildren("user")){
-		String path = new String(userElement.getChild("home").getText().getBytes("UTF-8"));
-		Directory homedir = createDirByPath(path);
-		User u = new User(homedir);
-		u.xmlImport(userElement);
+			for(Element userElement: firstElement.getChildren("user")){
+				String path = new String(userElement.getChild("home").getText().getBytes("UTF-8"));
+				Directory homedir = createDirByPath(path);
+				User u = new User(homedir);
+				u.xmlImport(userElement);
+			}
+
+			for(Element dirElement: firstElement.getChildren("dir")){
+				String name = dirElement.getChild("name").getText();
+				String path = dirElement.getChild("path").getText();
+				path = path + "/" + name;
+				Directory dir = createDirByPath(path);
+
+				Element owner = dirElement.getChild("owner");
+				User u = getUserByUsername(new String(owner.getText().getBytes("UTF-8")));
+				dir.setOwner(u);
+
+				dir.xmlImport(dirElement);
+			}
+
+			for(Element plainElement: firstElement.getChildren("plain")){
+				String name = plainElement.getChild("name").getText();
+				String path = plainElement.getChild("path").getText();
+				path = path + "/" + name;
+				PlainFile plain = createPlainByPath(path);
+
+				Element owner = plainElement.getChild("owner");
+				User u = getUserByUsername(new String(owner.getText().getBytes("UTF-8")));
+				plain.setOwner(u);
+
+				plain.xmlImport(plainElement);
+			}
+
+		} catch(UnsupportedEncodingException e){
+			throw new ImportDocumentException("in fs");
 		}
-
-		for(Element dirElement: firstElement.getChildren("dir")){
-		String name = dirElement.getChild("name").getText();
-		String path = dirElement.getChild("path").getText();
-		path = path + "/" + name;
-		Directory dir = createDirByPath(path);
-
-		Element owner = dirElement.getChild("owner");
-		User u = getUserByUsername(new String(owner.getText().getBytes("UTF-8")));
-		dir.setOwner(u);
-
-		dir.xmlImport(dirElement);
-		}
-
-		for(Element plainElement: firstElement.getChildren("plain")){
-		String name = plainElement.getChild("name").getText();
-		String path = plainElement.getChild("path").getText();
-		path = path + "/" + name;
-		PlainFile plain = createPlainByPath(path);
-
-		Element owner = plainElement.getChild("owner");
-		User u = getUserByUsername(new String(owner.getText().getBytes("UTF-8")));
-		plain.setOwner(u);
-
-		plain.xmlImport(plainElement);
-		}
-
-		for(Element linkElement: firstElement.getChildren("link")){
-		String name = linkElement.getChild("name").getText();
-		String path = linkElement.getChild("path").getText();
-		path = path + "/" + name;
-		Link link = createLinkByPath(path);
-
-		Element owner = linkElement.getChild("owner");
-		User u = getUserByUsername(new String(owner.getText().getBytes("UTF-8")));
-		link.setOwner(u);
-
-		link.xmlImport(linkElement);
-		}
-
-		for(Element appElement: firstElement.getChildren("app")){
-		String name = appElement.getChild("name").getText();
-		String path = appElement.getChild("path").getText();
-		path = path + "/" + name;
-		App app = createAppByPath(path);
-
-		Element owner = appElement.getChild("owner");
-		User u = getUserByUsername(new String(owner.getText().getBytes("UTF-8")));
-		app.setOwner(u);
-		app.xmlImport(appElement);
-		}
-		} catch(Exception e){
-		throw new ImportDocumentException("in fs");
-		}
-		}*/
+	}
 }
