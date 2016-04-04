2da5412aaac6c3dfa266b1efe4f0c824393309a0
Miguel Coimbra Vera <miguel.coimbra.vera@gmail.com>
2016-04-05 00:52:29 +0100

    Added Login skeleton so Service testing can begin.

--- src/main/java/pt/tecnico/myDrive/domain/Login.java
+++ src/main/java/pt/tecnico/myDrive/domain/Login.java
@@ -1,9 +1,28 @@
 package pt.tecnico.myDrive.domain;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 public class Login extends Login_Base {
 
-  public Login() {
-    super();
-  }
+	public Login() {
+		super();
+	}
 
+	public long createSession(String username, String password){
+		removeInvalidSessions();
+		long token = new BigInteger(64,new Random()).longValue();
+		//check if token is unique
+		return token;
+	}
+
+	public void removeInvalidSessions(){
+
+
+	}
+
+	public boolean checkToken(long token){
+
+		return true;
+	}
 }
