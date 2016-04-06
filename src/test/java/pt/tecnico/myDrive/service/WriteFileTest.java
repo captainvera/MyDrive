7c71363d3468f987df9d91041cc75ddde1f01bf9
pedroallenrevez <pedroallenrevez@gmail.com>
2016-04-06 23:13:24 +0100

    Added basic stubs for WriteFileTest

--- src/test/java/pt/tecnico/myDrive/service/WriteFileTest.java
+++ src/test/java/pt/tecnico/myDrive/service/WriteFileTest.java
@@ -1,21 +1,45 @@
-/**
- * 
- */
 package pt.tecnico.myDrive.service;
+import org.junit.Test;
+import pt.tecnico.myDrive.domain.FileSystem;
+import pt.tecnico.myDrive.domain.PlainFile;
+import pt.tecnico.myDrive.exceptions.FileUnknownException;
+import pt.tecnico.myDrive.exceptions.IllegalRemovalException;
+import pt.tecnico.myDrive.exceptions.InsufficientPermissionsException;
+import pt.tecnico.myDrive.exceptions.NotAPlainFileException;
 
-/**
- * @author miguel
- *
- */
 public class WriteFileTest extends AbstractServiceTest {
-
 	/* (non-Javadoc)
 	 * @see pt.tecnico.myDrive.service.AbstractServiceTest#populate()
 	 */
 	@Override
 	protected void populate() {
-		// TODO Auto-generated method stub
+		FileSystem fs = FileSystem.getInstance();
+		PlainFile readme = fs.createPlainFileByPath("/home/README");
+
 
 	}
 
+	private PlainFile getPlainFile(String path, String filename){
+	}	
+
+    @Test
+    public void success() {
+    }
+
+    @Test(expected = IllegalRemovalException.class)
+    public void invalidFileRemoval() {
+    }
+
+    @Test(expected = InsufficientPermissionsException.class)
+    public void invalidFileAccessPermissions() {
+    }
+
+    @Test(expected = NotAPlainFileException.class)
+    public void notAPlainFile() {
+	}
+
+
+    @Test(expected = FileUnknownException.class)
+    public void fileNonExistant() {
+    }
 }
