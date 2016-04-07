97cfcf4d571b61b3c9fcbc053927802393d46732
Miguel Coimbra Vera <miguel.coimbra.vera@gmail.com>
2016-04-07 15:18:21 +0100

    Created LoginService closes #34 closes #35

--- src/main/java/pt/tecnico/myDrive/services/LoginService.java
+++ src/main/java/pt/tecnico/myDrive/services/LoginService.java
@@ -1,18 +1,34 @@
 package pt.tecnico.myDrive.services;
 
+import pt.tecnico.myDrive.domain.User;
+import pt.tecnico.myDrive.exceptions.UserUnknownException;
+import pt.tecnico.myDrive.domain.FileSystem;
+import pt.tecnico.myDrive.exceptions.WrongPasswordException;
+
 public class LoginService extends myDriveService {
 
-	
+	private String _username;
+	private String _password;
+	private long _result;
+
 	/**
-	 *
+	 * Login Service Constructor
 	 */
-	public LoginService() {
+	public LoginService(String username, String password) {
 		super();
+		_username = username;
+		_password = password;
+
 	}
 
 	@Override
-	protected void dispatch() throws Exception {
-		// TODO Auto-generated method stub
-
+	protected void dispatch() throws UserUnknownException, WrongPasswordException{
+		FileSystem fs = FileSystem.getInstance();
+		_result = fs.login(_username, _password);
 	}
+
+	public long result(){
+		return _result;
+	}
+
 }
