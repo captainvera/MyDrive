5d79dc3fd43820afa383b772275239508d437b21
Miguel Coimbra Vera <miguel.coimbra.vera@gmail.com>
2016-04-11 03:08:57 +0100

    Added warnings to empty Login functions

--- src/main/java/pt/tecnico/myDrive/domain/Login.java
+++ src/main/java/pt/tecnico/myDrive/domain/Login.java
@@ -9,9 +9,14 @@ import java.util.TreeMap;
 import java.util.Calendar;
 import java.util.Date;
 import pt.tecnico.myDrive.domain.User;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
 
 public class Login extends Login_Base {
+  protected static final Logger log = LogManager.getRootLogger();
+
 	/**
 	 * Login constructor, receives logged user, currentDirectory and expiration date.
 	 */
@@ -50,27 +55,34 @@ public class Login extends Login_Base {
 	}
 	@Override
 	public Long getToken(){
+		log.warn("DO NOT USE Login.getToken() it only returns NULL");
 		return null;
 	}
 	@Override
 	public DateTime getExpirationDate(){
+		log.warn("DO NOT USE Login.getExpirationDate() it only returns NULL");
 		return null;
 	}
 	@Override
 	public void setExpirationDate(DateTime expirationDate) {
+		log.warn("DO NOT USE Login.setExpirationDate() it does nothing.");
 	}
 	@Override
 	public void setToken(Long token) {
+		log.warn("DO NOT USE Login.setToken() it does nothing.");
 	}
 	@Override
 	public FileSystem getFileSystem() {
+		log.warn("DO NOT USE Login.getFileSystem() it only returns NULL");
 		return null;
 	}
 	@Override
 	public void setFileSystem(FileSystem fileSystem) {
+		log.warn("DO NOT USE Login.setFileSystem() it does nothing.");
 	}
 	@Override
 	public void setUser(User user) {
+		log.warn("DO NOT USE Login.setUser() it does nothing.");
 	}
 	
 
