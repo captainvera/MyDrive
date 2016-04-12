4ca30c9266b6342dcbc8c6c548d8dcba144aac5e
pedroallenrevez <pedroallenrevez@gmail.com>
2016-04-12 12:47:11 +0100

    Implemented Link visitor

---
+++ src/main/java/pt/tecnico/myDrive/visitors/LinkVisitor.java
@@ -0,0 +1,38 @@
+package pt.tecnico.myDrive.visitors;
+
+import pt.tecnico.myDrive.domain.App;
+import pt.tecnico.myDrive.domain.Directory;
+import pt.tecnico.myDrive.domain.Link;
+import pt.tecnico.myDrive.domain.PlainFile;
+
+import pt.tecnico.myDrive.visitors.GenericVisitor;
+
+import pt.tecnico.myDrive.exceptions.NotALinkException;
+
+/**
+ * Simple directory visitor. Allows throws and exception if the element being visited is not a Link
+ */
+
+public class LinkVisitor implements GenericVisitor<Link> {
+
+  @Override
+  public Link visit(Directory dir) {
+    return null;
+  }
+
+  @Override
+  public Link visit(PlainFile pf) {
+    return null;
+  }
+
+  @Override
+  public Link visit(Link l) {
+    return l;
+  }
+
+  @Override
+  public Link visit(App a) {
+    return null;
+  }
+
+}
