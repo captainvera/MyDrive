4b5b9583157c2dc1c74f114a0033a5bf9fdb9cff
jcvicente <jcvicente16@gmail.com>
2016-04-14 07:09:30 +0100

    Added ChangeDirectoryService closes #46 closes #47

--- src/main/java/pt/tecnico/myDrive/services/ChangeDirectoryService.java
+++ src/main/java/pt/tecnico/myDrive/services/ChangeDirectoryService.java
@@ -1,18 +1,36 @@
 package pt.tecnico.myDrive.services;
 
+import pt.tecnico.myDrive.domain.FileSystem;
+import pt.tecnico.myDrive.exceptions.FileUnknownException;
+import pt.tecnico.myDrive.exceptions.NotADirectoryException;
+import pt.tecnico.myDrive.exceptions.InsufficientPermissionsException;
+import pt.tecnico.myDrive.exceptions.InvalidTokenException;
+import pt.tecnico.myDrive.exceptions.NotALinkException;
+
+
+
 public class ChangeDirectoryService extends myDriveService {
 
+	private long _token;
+	private String _dirpath;
+	private String _data;
 	/**
-	 *
+	 * Change Directory Service Constructor
 	 */
-	public ChangeDirectoryService() {
+	public ChangeDirectoryService(long token, String dirpath) {
 		super();
+		_token = token;
+		_dirpath = dirpath;
 	}
 
 	@Override
-	protected void dispatch() throws Exception {
-		// TODO Auto-generated method stub
-
+	protected void dispatch() throws FileUnknownException, NotADirectoryException, InsufficientPermissionsException, InvalidTokenException, NotALinkException {
+		FileSystem fs = FileSystem.getInstance();
+		_data = fs.changeDirectory(_token, _dirpath);
 	}
+
+	public String result() {
+    return _data;
+  }
 
 }
