1f0ea35acef6141969fd3ffda2dcf41f89d9dc92
Jorge <jpessoa.on@gmail.com>
2016-05-07 16:31:31 +0100

    Finished login command. Closes #84

--- src/main/java/pt/tecnico/myDrive/presentation/Login.java
+++ src/main/java/pt/tecnico/myDrive/presentation/Login.java
@@ -5,7 +5,7 @@ import java.util.TreeMap;
 
 public class Login extends MyDriveCommand {
 
-  public Login(Shell sh) { super(sh, "login", "logs a user"); }
+  public Login(MyDriveShell sh) { super(sh, "login", "logs a user"); }
 
   public void execute(String[] args) {
     LoginService ls = null;
@@ -17,10 +17,11 @@ public class Login extends MyDriveCommand {
       ls = new LoginService(args[0], args[1]);
 
     ls.execute();
+
     long token = ls.result();
     String username = args[0];
-    shell().setUser(username);
-    shell().setActiveToken(token);
-    shell().getTokens().put(username, token);
+
+    shell().addSession(username, token);
+    shell().setActiveSession(username);
   }
 }
