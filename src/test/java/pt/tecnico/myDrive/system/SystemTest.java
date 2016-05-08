4f1ebb950512fc005c09b017188e7de1d6d61988
jcvicente <jcvicente16@gmail.com>
2016-05-08 19:03:26 +0100

    Added some tests to system tests

--- src/test/java/pt/tecnico/myDrive/system/SystemTest.java
+++ src/test/java/pt/tecnico/myDrive/system/SystemTest.java
@@ -4,30 +4,25 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import pt.tecnico.myDrive.service.AbstractServiceTest ;
 import pt.tecnico.myDrive.presentation.*;
 
-public class SystemTest extends AbstractServiceTest {
+public class SystemTest extends AbstractSystemTest {
 
     private MyDriveShell sh;
 
     protected void populate() {
         sh = new MyDriveShell();
+        //TO DO
     }
 
     @Test
     public void success() {
-    /* ---------------- TO DO ----------------
-
-        new Import(sh).execute(new String[] { "other.xml" } );
-        new CreatePerson(sh).execute(new String[] { "Rui" } );
-        new CreateContact(sh).execute(new String[] { "Rui", "SOS", "112" } );
-        new List(sh).execute(new String[] { } );
-        new RemoveContact(sh).execute(new String[] { "Xana", "Xico" } );
-        new RemovePerson(sh).execute(new String[] { "Sofia" } );
-        new Export(sh).execute(new String[] { } );
-
-        ---------------- TO DO ----------------
-    */
+        new Login(sh).execute(new String[] { "exemplo", "1234" } );
+        new ChangeWorkingDirectory(sh).execute(new String[] { "/home/exemplo" } );
+        //new Environment(sh).execute(new String[] { "TO DO" } );
+        new Key(sh).execute(new String[] {} );
+        new ListDirectory(sh).execute(new String[] { "/home/exemplo" } );
+        new Write(sh).execute(new String[] { "/home/exemplo/ficheiro", "teste" } );
+        new Execute(sh).execute(new String[] { "/home/exemplo/ficheiro" } );
     }
 }
